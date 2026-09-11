91d24f6640fce9c346219a74212057e1f878c50a
Carlos O'Ryan <coryan@google.com>
2026-09-11 20:11:51 +0000

    feat: update librarian with serialize-enums-as-strings (#829)
    Update librarian to use strings when serializing enums. Fixed a number
    of unit / integration tests.

--- Sources/GoogleCloudDatastreamV1/PrivateConnection.swift
+++ Sources/GoogleCloudDatastreamV1/PrivateConnection.swift
@@ -186,12 +186,12 @@ public struct PrivateConnection: Codable, Equatable, GoogleCloudWKT._AnyPackable
     public func encode(to encoder: Encoder) throws {
       var container = encoder.singleValueContainer()
       switch self {
-      case .unspecified: return try container.encode(0)
-      case .creating: return try container.encode(1)
-      case .created: return try container.encode(2)
-      case .failed: return try container.encode(3)
-      case .deleting: return try container.encode(4)
-      case .failedToDelete: return try container.encode(5)
+      case .unspecified: return try container.encode("STATE_UNSPECIFIED")
+      case .creating: return try container.encode("CREATING")
+      case .created: return try container.encode("CREATED")
+      case .failed: return try container.encode("FAILED")
+      case .deleting: return try container.encode("DELETING")
+      case .failedToDelete: return try container.encode("FAILED_TO_DELETE")
       case .unknownIntValue(let v): return try container.encode(v)
       case .unknownStringValue(let v): return try container.encode(v)
       }
